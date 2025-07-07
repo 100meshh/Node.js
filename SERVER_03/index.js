428525da51966fef813ad150d46077fd23337057
Somesh Shukla <someshshuklacollege@gmail.com>
2025-07-07 15:32:29 +0530

    SOMESH-SERVER

--- SERVER_03/index.js
+++ SERVER_03/index.js
@@ -1,2 +1,18 @@
 const http=require("http")
-const myServer=http.createServer();
+const fs= require("fs")
+
+const myServer=http.createServer((req,res)=>{
+  const log =`${Date.now()}: ${req.url}new req. rec.\n`
+  fs.appendFile("log.txt",log,(err,data)=>{
+    switch(req.url){
+      case '/': res.end("Homepage");
+      break
+      case '/about': res.end("i am somesh shukla");
+      break
+      default : res.end("404")
+    }
+    res.end("hello from server")
+  })
+});
+
+myServer.listen(8000, ()=>console.log("somesh's server started"));
